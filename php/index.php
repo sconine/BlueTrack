08d5046fb4ff27d44cf4f79c1ca48c7118eff144
sconine <sconine@users.noreply.github.com>
2014-10-25 22:06:47 -0400

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,9 +1,7 @@
 <html>
 <head>BlueTrack</head>
 <body>
-Hi There!<br>
 <?php
-echo "hi Steve!";
 
 // Load my configuration
 $debug = true;
@@ -12,7 +10,6 @@ if ($debug) {echo "datastring = $datastring <br>\n";}
 $config = json_decode($datastring, true);
 if ($debug) {var_dump($config);}
 
-
 require '../vendor/autoload.php';
 use Aws\Common\Aws;
 
@@ -20,42 +17,38 @@ use Aws\Common\Aws;
 // make sure you specify the correct region as dynamo is region specific
 $aws = Aws::factory('/usr/www/html/BlueTrack/php/amazon_config.json');
 $client = $aws->get('DynamoDb');
-
-
 $tableName = "collector_data";
 
-# The Scan API is paginated. Issue the Scan request multiple times.
+echo "<table><tr><td>mac_id</td><td>collector_id</td><td>name</td><td>clock_offset</td><td>class</td><td>inq_on</td><td>scan_on</td></tr>"
+
+// The Scan API is paginated. Issue the Scan request multiple times.
 do {
-    echo "Scanning table $tableName" . PHP_EOL;
     $request = array(
         "TableName" => $tableName,
         "Limit" => 20
     );
 
-    # Add the ExclusiveStartKey if we got one back in the previous response
+    // Add the ExclusiveStartKey if we got one back in the previous response
     if(isset($response) && isset($response['LastEvaluatedKey'])) {
         $request['ExclusiveStartKey'] = $response['LastEvaluatedKey'];
     }
     $response = $client->scan($request);
-    echo "new page<hr>";
 
     foreach ($response['Items'] as $key => $value) {
-      //var_dump($value);
-      echo "mac_id: " . $value['mac_id']["S"] . "<br>";
+      echo "<tr><td>" . $value['mac_id']["S"] . "</td>";
+      echo "<td>" . $value['collector_id']["S"] . "</td>";
+      echo "<td>" . $value['name']["SS"] . "</td>";
+      echo "<td>" . $value['clock_offset']["S"] . "</td>";
+      echo "<td>" . $value['class']["SS"] . "</td>";
+      echo "<td>" . $value['inq_on']["SS"] . "</td>";
+      echo "<td>" . $value['scan_on']["SS"] . "</td></tr>";
     }
 } while(isset($response['LastEvaluatedKey'])); 
 //If there is no LastEvaluatedKey in the response, there are no more items matching this Scan invocation
 
-//'mac_id'      => array("S" => $mac),
-//'collector_id'      => array("S" => $collector_id)
-//"name" => array("SS" => $name),
-//"clock_offset" =>  array("SS" => $clock_offset),
-//"class" => array("SS" => $class),
-//"inq_on" => array("NS" => $inq_on),
-//"scan_on" => array("NS" => $scan_on),
 
 
-echo 'Made it here!<br>';
+echo '</table>Made it here!<br>';
 
 ?>
 
