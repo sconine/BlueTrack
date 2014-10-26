ab5d175e180dcecb1fc2c8a8f83e49e7a3630bed
sconine <sconine@users.noreply.github.com>
2014-10-25 20:11:09 -0400

    Update scan_signals.php

--- php/scan_signals.php
+++ php/scan_signals.php
@@ -263,7 +263,7 @@ function gethostmacaddr() {
 	exec("ifconfig", $out);
 	var_dump($out);
 	foreach ($out as $i => $v) {
-		if (substr($v, 0, 4) = 'eth0') {
+		if (substr($v, 0, 4) == 'eth0') {
 			$mm = substr($v, strpos($v, 'HWaddr') + 7, 17);
 		}
 	}
