74e7c3b395f8e5160f4cd03f24b9c35cb80b0401
sconine <sconine@users.noreply.github.com>
2014-10-29 21:22:58 -0400

    Update analyze_one.php

--- php/analyze_one.php
+++ php/analyze_one.php
@@ -31,7 +31,7 @@ if(!empty($_REQUEST['mac'])) {$mac = $_REQUEST['mac'];}
 if (isset($_REQUEST['col'])) {$collector_id = $_REQUEST['col'];} else {$collector_id = 'b8:27:eb:3a:0b:aa';}
 
 // Make sure they look safe
-$pattern = '/^[a-zA-ZvV0-9,]+$/';
+$pattern = '/^[a-zA-ZvV0-9,:]+$/';
 if (preg_match($pattern, $mac) == 0) {$mac = '00:11:B1:08:97:3D';}
 if (preg_match($pattern, $collector_id) == 0) {$collector_id = 'b8:27:eb:3a:0b:aa';}
 
