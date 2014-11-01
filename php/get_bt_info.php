3251914ffd4da9ef263c6a31614e234bb322dbb5
sconine <sconine@users.noreply.github.com>
2014-10-31 21:31:48 -0400

    Update get_bt_info.php

--- php/get_bt_info.php
+++ php/get_bt_info.php
@@ -5,14 +5,6 @@
 $mac = '00:22:58';
 $ch = curl_init();
 
-<form action="/cgi-bin/ouisearch" class="bodycopy" method="post"><p><strong>Search the Public MA-L Listing</strong><br>
-              Search for:
-              <input name="x" size="30" type="text" value="">
-              <input name="submit2" type="submit" value="Search!">
-              <input name="reset" type="reset" value="clear field">
-              </p></form>
-              
-
 curl_setopt($ch, CURLOPT_URL,"http://http://standards.ieee.org/cgi-bin/ouisearch");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,
