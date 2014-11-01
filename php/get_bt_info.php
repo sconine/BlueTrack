710f87c3638c59d012d727b3db4248de84581f23
sconine <sconine@users.noreply.github.com>
2014-10-31 22:15:54 -0400

    Update get_bt_info.php

--- php/get_bt_info.php
+++ php/get_bt_info.php
@@ -33,9 +33,8 @@ if ($start > 10 && $end > $start) {
             $data = str_replace('(base 16)', '', $data);
             $cc = str_replace('-', '', $mac);
             $data = str_replace($cc, '', $data);
-            $data = preg_replace("[\s^\n]{2,}", "", $data);
+            $data = preg_replace("/[\s^\n]{2,}/", "", $data);
 } 
-
 
 echo "start: $start <br>\n";
 echo "end: $end <br><hr><hr>\n";
