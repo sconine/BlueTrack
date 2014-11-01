80067a93a8897cb56c824c922b85a62f60702659
sconine <sconine@users.noreply.github.com>
2014-11-01 18:26:59 -0400

    Update index.php

--- php/index.php
+++ php/index.php
@@ -20,7 +20,40 @@ include 'index_include.php';
 	</div>
 	
 	<div class="row">
-		<div class="col-md-4">Put for to filter graphs here</div>
+		<div class="col-md-4">
+			<div class="well bs-component">
+				<form method="GET" action="index.php" class="form-horizontal">
+				<input type="hidden" name="bust" value="<?php echo time();?>"> 
+				<fieldset>
+					<legend>Device Type Key</legend>
+					<div class="form-group">
+						<label for="filterForm" class="col-lg-2 control-label">Filter Devices</label>
+						<div class="col-lg-10">
+							<input type="checkbox" name="multi_day" value="d" <?php echo checkit($multi_day_f);?>> Show Multi Day Devices Only
+							<input type="text" name="day_count" size="4" value="<?php echo $day_count_f;?>"> Min Days seen<br>
+							<input type="checkbox" name="type[]" value="M" <?php echo ischecked('M', $type_f);?>> M = Mobile Phone<br>
+							<input type="checkbox" name="type[]" value="H" <?php echo ischecked('H', $type_f); ?>> H = Human<br>
+							<input type="checkbox" name="type[]" value="V" <?php echo ischecked('V', $type_f); ?>> V = Vehicle<br>
+							<input type="checkbox" name="type[]" value="A" <?php echo ischecked('A', $type_f); ?>> A = Apple Device<br>
+							<input type="checkbox" name="type[]" value="C" <?php echo ischecked('C', $type_f); ?>> C = Computer<br>
+							<input type="checkbox" name="type[]" value="G" <?php echo ischecked('G', $type_f); ?>> G = GPS<br>
+							<input type="checkbox" name="type[]" value="T" <?php echo ischecked('T', $type_f); ?>> T = TV Device<br>
+							<input type="checkbox" name="type[]" value="S" <?php echo ischecked('S', $type_f); ?>> S = Music Device<br>
+							<input type="checkbox" name="type[]" value="U" <?php echo ischecked('U', $type_f); ?>> U = Unknown<br>
+							<input type="checkbox" name="type[]" value="X" <?php echo ischecked('X', $type_f); ?>> X = Not Set<br>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-lg-10 col-lg-offset-2">
+							<button class="btn btn-default">Cancel</button>
+							<button type="submit" class="btn btn-primary">Submit</button>
+							<input type="submit" name="update" value="update">
+						</div>
+					</div>					
+				</fieldset>
+				</form>
+			</div>
+		</div>
 		<div class="col-md-4">Put info about collectors here</div>
 		<div class="col-md-4"><div id="byclass"></div></div>
 	</div>
@@ -39,23 +72,7 @@ echo "<tr><td>Seen in Last Hour</td><td>" . count($last_hour) . "</td></tr>";
 echo "</table><br>";
 ?>
 
-<form method="GET" action="index.php">
-<b>Device Type Key</b><br>
-<input type="hidden" name="bust" value="<?php echo time();?>"> 
-<input type="checkbox" name="multi_day" value="d" <?php echo checkit($multi_day_f);?>> Show Multi Day Devices Only
-<input type="text" name="day_count" size="4" value="<?php echo $day_count_f;?>"> Min Days seen<br>
-<input type="checkbox" name="type[]" value="M" <?php echo ischecked('M', $type_f);?>> M = Mobile Phone<br>
-<input type="checkbox" name="type[]" value="H" <?php echo ischecked('H', $type_f); ?>> H = Human<br>
-<input type="checkbox" name="type[]" value="V" <?php echo ischecked('V', $type_f); ?>> V = Vehicle<br>
-<input type="checkbox" name="type[]" value="A" <?php echo ischecked('A', $type_f); ?>> A = Apple Device<br>
-<input type="checkbox" name="type[]" value="C" <?php echo ischecked('C', $type_f); ?>> C = Computer<br>
-<input type="checkbox" name="type[]" value="G" <?php echo ischecked('G', $type_f); ?>> G = GPS<br>
-<input type="checkbox" name="type[]" value="T" <?php echo ischecked('T', $type_f); ?>> T = TV Device<br>
-<input type="checkbox" name="type[]" value="S" <?php echo ischecked('S', $type_f); ?>> S = Music Device<br>
-<input type="checkbox" name="type[]" value="U" <?php echo ischecked('U', $type_f); ?>> U = Unknown<br>
-<input type="checkbox" name="type[]" value="X" <?php echo ischecked('X', $type_f); ?>> X = Not Set<br>
-<input type="submit" name="update" value="update">
-</form>
+
 </body>
 </html>
 
