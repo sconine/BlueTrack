7fa20883855d07ac12a87cf42527ae65375086a9
sconine <sconine@users.noreply.github.com>
2014-11-01 21:09:28 -0400

    Update index.php

--- php/index.php
+++ php/index.php
@@ -74,13 +74,13 @@ include 'index_include.php';
 				<p>
 				<?php
 					foreach ($collectors as $id => $v) {
-						$tip = str_pad('Checkins: ' . number_format($v['collector_checkin_count']), 40, "&nbsp;");
-						$tip .= str_pad('Last Seen: ' . date("Y-m-d h:i a", $v['collector_last_checkin']), 40, "&nbsp;");
+						$tip = str_pad('Checkins: ' . number_format($v['collector_checkin_count']), 40, " ");
+						$tip .= str_pad('Last Seen: ' . date("Y-m-d h:i a", $v['collector_last_checkin']), 40, " ");
 						$tip .= 'IP: ' . $v['collector_private_ip'];
 						echo '<button type="button" class="btn btn-default" ';
 						echo 'data-toggle="tooltip" data-placement="right" title="" ';
 						echo 'data-original-title="';
-						echo htmlentities($tip);
+						echo $tip;
 						echo '">' . $id . '</button>';
 	
 					}
