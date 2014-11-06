35ff53cde73970b747227ad262bafbb4dcd57e56
sconine <sconine@users.noreply.github.com>
2014-11-05 20:24:28 -0500

    Update index_include.php

--- php/index_include.php
+++ php/index_include.php
@@ -271,16 +271,11 @@ do {
         
         // See if there are filters applied
         $skip_it = false;
-        echo $day_count_f . ' day_count_f <br>';
-        echo count($seen_days[$mac]) . ' seen_days <br>';
-        echo $seen_count . ' seen_count <br>';
-        echo $total_count_f . ' total_count_f <br>';
         if ($day_count_f > 0) {if (count($seen_days[$mac]) < $day_count_f) {$skip_it = true;}}
         if ($total_count_f > 0) {if ($seen_count < $total_count_f) {$skip_it = true;}}
         if ($skip_it) {echo 'skip_it is true <br>';} else {echo 'skip_it is false <br>';}
 
         if (! $skip_it) {
-            echo 'Addint to TOP! <br>';
             // create an array to use in the bubble chart if not filters
             $top[$mac] = $seen_count;
             $displayed_count++;
