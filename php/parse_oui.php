fcda65e6fc3ae7a68c60d333003bdf07b20d3779
sconine <sconine@users.noreply.github.com>
2014-11-20 17:23:19 -0500

    Update and rename parse_out.php to parse_oui.php

---
+++ php/parse_oui.php
@@ -0,0 +1,57 @@
+<?php
+// First run wget http://www.ieee.org/netstorage/standards/oui.txt to get the latest Bluetooth 
+// mac address license file
+
+$f = file("/usr/www/html/BlueTrack/data/oui.txt");
+$pattern = '/^\s*([a-zA-Z0-9\-]{8})\s+.*$/';
+$statesp = '/^([A-Z ]+)\s+([A-Z]{2})\s+([0-9-]{5,10})$/';
+$replacement = '$1';
+$data = array();
+$thisline = '';
+$row = 0;
+$all = array();
+
+foreach ($f as $i => $line) {
+  if (preg_match($pattern, $line) != 0) {
+    // Process the last one we just found
+    if ($row != 0) {
+      $addr_rows = count($data['address']) - 1;
+      $all[$mac]['company'] = $data['company'];
+      $all[$mac]['country'] = $data['address'][$addr_rows];
+      if ($data['country'] == 'UNITED STATES') {
+        $st = $data['address'][$addr_rows - 1];
+        if (preg_match($pattern, $st, $matches) != 0) {
+          $all[$mac]['city'] = $matches[1];
+          $all[$mac]['state'] = $matches[2];
+          $all[$mac]['zip'] = $matches[3];
+          $addr_rows = $addr_rows - 1;
+        }
+      }
+      for ($i = 0; $i < $addr_rows ; $i++) {$all[$mac]['address'][] = $data['address'][$i];}
+      var_dump($all);
+      exit;
+    }
+    
+    $mac = preg_replace($pattern, $replacement, $string);
+    unset($data);
+    $data = array();
+    $row = 0;
+  } else {
+    if (!empty(trim($line))) {
+      $thisline = strtoupper(substr($line, 24));
+      if ($row == 0) {$data['company'] = $thisline;}
+      else {$data['address'][] = $thisline;}
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+?>
