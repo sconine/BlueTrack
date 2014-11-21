b84b34d45b6ac8e5bfbdc7f91f3b379dddb3ff15
sconine <sconine@users.noreply.github.com>
2014-11-21 18:03:57 -0500

    Update parse_oui.php

--- php/parse_oui.php
+++ php/parse_oui.php
@@ -24,6 +24,7 @@ foreach ($f as $i => $line) {
         $st = $data['address'][$addr_rows - 1];
         echo "state: $st \n";
         if (preg_match($statesp, $st, $matches) != 0) {
+          echo "#########ggggggggggggggggggg############ \n";
           $all[$mac]['city'] = $matches[1];
           $all[$mac]['state'] = $matches[2];
           $all[$mac]['zip'] = $matches[3];
