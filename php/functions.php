13a079148bdc3181bddd3049642d16073684f83b
sconine <sconine@users.noreply.github.com>
2014-11-24 17:42:09 -0500

    Update functions.php

--- php/functions.php
+++ php/functions.php
@@ -106,7 +106,9 @@ function create_select($name, $arr, $def, $multi, $size) {
 	foreach ($arr as $i => $v) {
 		$to_ret .= '<option value="' . htmlentities($i) . '"';
 		if (in_array($v, $def)) {$to_ret .= ' selected ';}
-		$to_ret .= ">" . htmlentities($v) . "</option>\n";
+		$disp = htmlentities($v);
+		if (strlen($disp) > 32) {$disp = substr($disp, 0, 29) . '...';}
+		$to_ret .= ">" . htmlentities($disp) . "</option>\n";
 	}
 	$to_ret .= "</select>\n";
 	return $to_ret;
