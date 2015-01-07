f2e11014cc47302df3005c8be5454eb851fb1064
sconine <sconine@users.noreply.github.com>
2015-01-07 17:58:09 -0500

    Update sync_dynamo.php

--- php/sync_dynamo.php
+++ php/sync_dynamo.php
@@ -24,14 +24,9 @@ use Aws\Common\Aws;
 // Loop through the dynamo tables and load the data into MySQL
 $aws = Aws::factory('/usr/www/html/BlueTrack/php/amazon_config.json');
 $client = $aws->get('DynamoDb');
-$tableName = "collector_data";
 
 // Setup to run through a table 100 pages at a time
-$request = array(
-    "TableName" => $tableName,
-    //"ConditionalOperator" => 'OR',
-    "Limit" => 100
-);
+$request = array("TableName" => "collector_data","Limit" => 100);
 
 do {
     // Add the ExclusiveStartKey if we got one back in the previous response
@@ -58,27 +53,86 @@ do {
         foreach ($seen as $i => $v) {    
             $sql .= 'REPLACE INTO device_scans (mac_id, collector_id, seen)';
             $sql .= ' VALUES (' . sqlq($mac,0) . ',' .
-            $sql .= ' VALUES (' . sqlq($collector_id,0) . ',' .
-            $sql .= ' VALUES (' . sqlq($v,1) . '); ';
+                    sqlq($collector_id,0) . ',' .
+                    sqlq($v,1) . '); ';
         }
         
         if ($class != 'n/a') {
             $hex = str_replace("0x", "", $class);
             $class_det = get_bt_class_info($hex, $mdcs, $mdc, $msc, $min_sc);
     
-            $sql .= 'REPLACE INTO varchar (class, short_major_type, major_type, service_class, device_type)';
+            $sql .= 'REPLACE INTO class_description  (class, short_major_type, major_type, service_class, device_type)';
             $sql .= ' VALUES (' . sqlq($class,0) . ',' .
-            $sql .= ' VALUES (' . sqlq($mdcs,0) . ',' .
-            $sql .= ' VALUES (' . sqlq($mdc,0) . ',' .
-            $sql .= ' VALUES (' . sqlq(json_encode($msc),0) . ',' .
-            $sql .= ' VALUES (' . sqlq(json_encode($min_sc),0) . '); ';
+                    sqlq($mdcs,0) . ',' .
+                    sqlq($mdc,0) . ',' .
+                    sqlq(json_encode($msc),0) . ',' .
+                    sqlq(json_encode($min_sc),0) . '); ';
         }
-
+        
+    	if ($debug) {echo "Running: $sql\n";}
+    	if (!$mysqli->query($sql)) {die("Insert Failed: (" . $mysqli->errno . ") " . $mysqli->error);}
     }
 } while(isset($response['LastEvaluatedKey'])); 
 
 // This comes from mac_data table
         //$full_data[$mac][$collector_id]['mac_info'] = isset($value['mac_info']["S"]) ? $value['mac_info']["S"] : 'n/a';
 
+// Setup to run through a table 100 pages at a time
+$request = array("TableName" => "mac_data","Limit" => 100);
 
+do {
+    // Add the ExclusiveStartKey if we got one back in the previous response
+    if(isset($response) && isset($response['LastEvaluatedKey'])) {
+        $request['ExclusiveStartKey'] = $response['LastEvaluatedKey'];
+    }
+    $response = $client->scan($request);
+    $sql = '';
+    
+    foreach ($response['Items'] as $key => $value) {
+        $company_name = $value['company_name']["S"];
+        $address = isset($value['address']["S"]) ? $value['address']["S"] : 'n/a';
+        $city = isset($value['city']["S"]) ? $value['city']["S"] : 'n/a';
+        $country = isset($value['country']["S"]) ? $value['country']["S"] : 'n/a';
+        $state = isset($value['state']["S"]) ? $value['state']["S"] : 'n/a';
+        $zip = isset($value['zip']["S"]) ? $value['zip']["S"] : 'n/a';
+        $macs = isset($value['macs']["SS"]) ? $value['macs']["SS"] : array();
+        $manu_id = get_manu_id($company_name, &$mysqli);
+
+        if (count($manu_id) == 0) {
+            $msql = 'INSERT INTO manufacturers (company_name, address, city, country, state, zip)';
+            $msql .= ' VALUES (' . sqlq($mac,0) . ',' .
+                    sqlq(base_mac($mac),0) . ',' .
+                    sqlq($class,0) . ',' .
+                    sqlq($name,0) . ',' .
+                    sqlq($type,0) . '); ';
+        	if ($debug) {echo "Running: $sql\n";}
+        	if (!$mysqli->query($msql)) {die("Insert Failed: (" . $mysqli->errno . ") " . $mysqli->error);}
+        	
+            // Get the ID we just created (yea there's probably a way to do this in 1 step...)
+            $manu_id = get_manu_id($company_name, &$mysqli);
+        }
+        
+        // Store Macs this manu is assocaited with
+        foreach ($macs as $i => $mac_root) {    
+            $sql . = 'INSERT INTO mac_roots (manu_id, mac_root)';
+            $sql .= ' VALUES (' . sqlq($manu_id,1) . ',' . sqlq($mac_root,0) . ');';
+        }
+    }
+
+    // Insert all the mac root mappings we've got so far
+	if ($debug) {echo "Running: $sql\n";}
+	if (!$mysqli->query($sql)) {die("Insert Failed: (" . $mysqli->errno . ") " . $mysqli->error);}
+} while(isset($response['LastEvaluatedKey'])); 
+
+
+function get_manu_id($company_name, &$mysqli) {
+    // See if we've created this company before
+    $sql = 'SELECT manu_id FROM manufacturers WHERE company_name=' . sqlq($company_name,0) . ';';
+	$manu_id = query_to_array($sql, $mysqli);
+	if (count($manu_id) == 0) {
+	    return 0;
+	} else {
+	    return $manu_id[0];
+	}
+}
 ?>
