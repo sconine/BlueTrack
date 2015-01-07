39a3f1afabb91a2c6997a8c1824ef2d1775903f2
sconine <sconine@users.noreply.github.com>
2015-01-07 14:55:42 -0500

    Update sync_dynamo.php

--- php/sync_dynamo.php
+++ php/sync_dynamo.php
@@ -33,29 +33,8 @@ $sql = 'CREATE TABLE IF NOT EXISTS devices ('
 if (!$mysqli->query($sql)) {die("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);}
 if ($debug) {echo 'devices table Exists'. "\n";}
 
-// Build the devices table schema on the fly
-$sql = 'CREATE TABLE IF NOT EXISTS device_scans ('
-. ' id INTEGER AUTO_INCREMENT UNIQUE KEY, '
-. ' mac_id varchar(32) NOT NULL, '
-. ' device_id varchar(32) NOT NULL, '
-. ' seen int NOT NULL, '
-. ' PRIMARY KEY (mac_id, device_id, seen), '
-. ' INDEX(seen), INDEX(device_id));';
-if (!$mysqli->query($sql)) {die("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);}
-if ($debug) {echo 'device_scans table Exists'. "\n";}
-
-// Build the devices table schema on the fly
-$sql = 'CREATE TABLE IF NOT EXISTS device_scans ('
-. ' id INTEGER AUTO_INCREMENT UNIQUE KEY, '
-. ' mac_id varchar(32) NOT NULL, '
-. ' device_id varchar(32) NOT NULL, '
-. ' seen int NOT NULL, '
-. ' PRIMARY KEY (mac_id, device_id, seen), '
-. ' INDEX(seen), INDEX(device_id));';
-if (!$mysqli->query($sql)) {die("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);}
-if ($debug) {echo 'device_scans table Exists'. "\n";}
-
-// Build the devices table schema on the fly
+// Build the class_description table schema on the fly
+// stores the description of what a class ID stands for in english
 $sql = 'CREATE TABLE IF NOT EXISTS class_description ('
 . ' id INTEGER AUTO_INCREMENT UNIQUE KEY, '
 . ' class varchar(32) NOT NULL, '
@@ -63,9 +42,39 @@ $sql = 'CREATE TABLE IF NOT EXISTS class_description ('
 . ' long varchar(512) NOT NULL, '
 . ' PRIMARY KEY (class);';
 if (!$mysqli->query($sql)) {die("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);}
+if ($debug) {echo 'class_description table Exists'. "\n";}
+
+// Build the device_scans table schema on the fly
+$sql = 'CREATE TABLE IF NOT EXISTS device_scans ('
+. ' id INTEGER AUTO_INCREMENT UNIQUE KEY, '
+. ' mac_id varchar(32) NOT NULL, '
+. ' device_id varchar(32) NOT NULL, '
+. ' seen int NOT NULL, '
+. ' PRIMARY KEY (mac_id, device_id, seen), '
+. ' INDEX(seen), INDEX(device_id));';
+if (!$mysqli->query($sql)) {die("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);}
 if ($debug) {echo 'device_scans table Exists'. "\n";}
 
+// Build the manufacturers table schema on the fly
+$sql = 'CREATE TABLE IF NOT EXISTS manufacturers ('
+. ' manu_id INTEGER AUTO_INCREMENT UNIQUE KEY, '
+. ' company_name varchar(255) NULL, '
+. ' address varchar(255) NULL, '
+. ' city varchar(64) NULL, '
+. ' country varchar(128) NULL, '
+. ' state varchar(64) NULL, '
+. ' zip varchar(32) NULL, '
+. ' PRIMARY KEY (manu_id));';
+if (!$mysqli->query($sql)) {die("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);}
+if ($debug) {echo 'manufacturers table Exists'. "\n";}
 
+// Build the mac_roots table schema on the fly
+$sql = 'CREATE TABLE IF NOT EXISTS mac_roots ('
+. ' manu_id INTEGER NOT NULL, '
+. ' mac_root varchar(32) NOT NULL, '
+. ' PRIMARY KEY (manu_id, mac_root);';
+if (!$mysqli->query($sql)) {die("Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error);}
+if ($debug) {echo 'mac_roots table Exists'. "\n";}
 
 
 // connect to S3 and get a list of files we are storeing
