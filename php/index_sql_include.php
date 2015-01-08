b910577105f128297cf0e62d2e82372758c6f44d
sconine <sconine@users.noreply.github.com>
2015-01-08 11:57:43 -0500

    Update index_sql_include.php

--- php/index_sql_include.php
+++ php/index_sql_include.php
@@ -18,13 +18,28 @@ use Aws\Common\Aws;
 $aws = Aws::factory('/usr/www/html/BlueTrack/php/amazon_config.json');
 $client = $aws->get('DynamoDb');
 
+// Setup filters
+$type_f = array();
+if(!empty($_REQUEST['type'])) {$type_f = $_REQUEST['type'];}
+$pattern = '/^[a-zA-ZvV0-9,]+$/';
+if (preg_match($pattern, implode(",", $type_f)) == 0) {$type_f = array();}
+
 // List of type filters from class table
-$sql = 'SELECT DISTINCT short_major_type FROM class_description;';
+$sql = 'SELECT class, short_major_type, device_type FROM class_description WHERE  short_major_type IS NOT NULL ORDER BY  short_major_type, device_type;';
 $type_ar = query_to_array($sql, $mysqli);
 if (count($type_ar) == 0) {
   $type_desc = array();
 } else {
-  foreach ($type_ar as $i => $v) {$type_desc[$v['short_major_type']] = $v['short_major_type'];}
+  $class_array = array();
+  foreach ($type_ar as $i => $v) {
+    $dt = json_decode($v['device_type']);
+    $desc = $v['short_major_type'];
+    if (count($dt) == 0) {$desc .= ' - Other';}
+    else {$desc .= ' - ' . $dt[0];}
+    if (isset($class_array[$desc])) {$class_array[$desc] .= ',' . $v['class'];}
+    else {$class_array[$desc] = $v['class'];}
+  }
+  foreach ($class_array as $i => $v) {$type_desc[$v] = $i;}
 }
 
 ?>
